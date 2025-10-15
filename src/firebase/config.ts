b99318ba68aaff8ab866df059018463f809f1698
Ahmed Ben Mariam <benmariamahmed@gmail.com>
2025-10-15 14:01:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3161964601-a5bd8",
+  "appId": "1:312056907023:web:24fc29c610544088f02a7a",
+  "apiKey": "AIzaSyCFtdI0W8q91vRu1FmSyAwtZQ5ywvqrE-c",
+  "authDomain": "studio-3161964601-a5bd8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "312056907023"
+};
