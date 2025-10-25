09007a240d560c41991475ae7231ce3cd745bd4a
Ahmed Ben Mariam <benmariamahmed@gmail.com>
2025-10-25 14:55:45 +0000

    les types sont bien recupérés dans Détail des Bacs Manquants peux tu voi

--- src/components/app/redelivery-details-dialog.tsx
+++ src/components/app/redelivery-details-dialog.tsx
@@ -34,7 +34,7 @@ const countBacs = (task: Tache) => {
     return task.articles.reduce((acc, article) => {
         if (article.type === 'BAC_SEC') acc.secs++;
         else if (article.type === 'BAC_FRAIS') acc.frais++;
-        else if (article.type === 'SURGELE') acc.surgeles++;
+        else if (article.type === 'BAC_SURGELE') acc.surgeles++; // Corrected type
         return acc;
     }, { secs: 0, frais: 0, surgeles: 0 });
 };
@@ -120,5 +120,3 @@ export function RedeliveryDetailsDialog({
     </Dialog>
   );
 }
-
-    
