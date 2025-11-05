62e2b12b2622f462de71dd8675c27236c141d389
Ahmed Ben Mariam <benmariamahmed@gmail.com>
2025-11-05 23:41:34 +0000

    ok

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-3161964601-a5bd8",
-  "appId": "1:312056907023:web:24fc29c610544088f02a7a",
-  "apiKey": "AIzaSyCFtdI0W8q91vRu1FmSyAwtZQ5ywvqrE-c",
-  "authDomain": "studio-3161964601-a5bd8.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "312056907023"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
